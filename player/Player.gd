64577f00731b262b8bc740348cd293466de33397
LancerFate <matias-leandro@outlook.com>
2019-03-29 22:23:40 -0300

    I put the run animation to the Player

--- player/Player.gd
+++ player/Player.gd
@@ -12,10 +12,15 @@ func _physics_process(delta):
 	
 	if Input.is_action_pressed("ui_right"):
 		motion.x = SPEED
+		$polygons.scale.x = 1
+		$polygons/AnimationPlayer.play("run")
 	elif Input.is_action_pressed("ui_left"):
 		motion.x = -SPEED
+		$polygons.scale.x = -1
+		$polygons/AnimationPlayer.play("run")
 	else:
 		motion.x = 0
+		$polygons/AnimationPlayer.play("idle")
 	
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
